66b44cbbe191179cf2809fb31b998bc74a07131c
Szkupien Péter <peti.szkupien@gmail.com>
2024-02-04 22:20:20 +0100

    Add Engedely Request and EventArgs tests

---
+++ Gyermekvasut.Halozat.Tests/EngedelyAdas/EngedelyAdasEventArgsTests.cs
@@ -0,0 +1,43 @@
+﻿using Gyermekvasut.Grpc;
+using Gyermekvasut.Grpc.Server;
+using Gyermekvasut.Halozat.EventArgsNS;
+using Gyermekvasut.Tests.Util;
+
+namespace Gyermekvasut.Halozat.Tests.EngedelyAdas;
+
+[TestClass]
+public class EngedelyAdasEventArgsTests
+{
+    [TestMethod]
+    public void FromGrpcEventArgs_WhenRequestHasUtolsoVonatAndTipusIsAzonosIranyu_ShouldThrow()
+    {
+        // Arrange
+        var grpcEventArgs = new GrpcRequestEventArgs<EngedelyAdasRequest>(new EngedelyAdasRequest
+        {
+            Kuldo = GrpcAllomasNev.Szechenyihegy,
+            Tipus = GrpcEngedelyAdasTipus.AzonosIranyu,
+            UtolsoVonat = VonatTestsUtil.PAROS_VONATSZAM,
+            Vonatszam = VonatTestsUtil.PARATLAN_VONATSZAM,
+            Nev = TelefonTestsUtil.TEST_NEV
+        });
+        // Act and assert
+        var exception = Assert.ThrowsException<ArgumentException>(() => EngedelyAdasEventArgs.FromGrpcEventArgs(grpcEventArgs));
+        Assert.AreEqual("Az utolsó vonatnak pontosan akkor kell hiányoznia, ha azonos irányú az engedélyadás", exception.Message);
+    }
+
+    [TestMethod]
+    public void FromGrpcEventArgs_WhenRequestNoUtolsoVonatAndTipusIsEllenkezoIranyu_ShouldThrow()
+    {
+        // Arrange
+        var grpcEventArgs = new GrpcRequestEventArgs<EngedelyAdasRequest>(new EngedelyAdasRequest
+        {
+            Kuldo = GrpcAllomasNev.Szechenyihegy,
+            Tipus = GrpcEngedelyAdasTipus.EllenkezoIranyu,
+            Vonatszam = VonatTestsUtil.PARATLAN_VONATSZAM,
+            Nev = TelefonTestsUtil.TEST_NEV
+        });
+        // Act and assert
+        var exception = Assert.ThrowsException<ArgumentException>(() => EngedelyAdasEventArgs.FromGrpcEventArgs(grpcEventArgs));
+        Assert.AreEqual("Az utolsó vonatnak pontosan akkor kell hiányoznia, ha azonos irányú az engedélyadás", exception.Message);
+    }
+}
---
+++ Gyermekvasut.Halozat.Tests/GrpcRequestFactoryTests.cs
@@ -0,0 +1,68 @@
+﻿using Gyermekvasut.Halozat.Factory;
+using Gyermekvasut.Modellek.AllomasNS;
+using Gyermekvasut.Modellek.Telefon;
+using Gyermekvasut.Tests.Util;
+
+namespace Gyermekvasut.Halozat.Tests;
+
+[TestClass]
+public class GrpcRequestFactoryTests
+{
+    [TestMethod]
+    public void CreateEngedelyKeresRequest_WhenHasUtolsoVonatAndTipusIsAzonosIranyu_ShouldThrow()
+    {
+        // Arrange
+        var allomasNev = AllomasNev.Szechenyihegy;
+        var utolsoVonat = VonatTestsUtil.PAROS_VONATSZAM;
+        var vonatszam = VonatTestsUtil.PARATLAN_VONATSZAM;
+        var ido = TelefonTestsUtil.TEST_IDO;
+        var nev = TelefonTestsUtil.TEST_NEV;
+        // Act and assert
+        var exception = Assert.ThrowsException<ArgumentException>(() => GrpcRequestFactory.CreateEngedelyKeresRequest(allomasNev,
+            EngedelyKeresTipus.AzonosIranyuVolt, utolsoVonat, vonatszam, ido, nev));
+        Assert.AreEqual("Az utolsó vonatnak pontosan akkor kell hiányoznia, ha azonos irányú az engedélykérés", exception.Message);
+    }
+
+    [DataTestMethod]
+    [DataRow(EngedelyKeresTipus.EllenkezoIranyuVolt)]
+    [DataRow(EngedelyKeresTipus.EllenkezoIranyuVan)]
+    public void CreateEngedelyKeresRequest_WhenNoUtolsoVonatAndTipusIsEllenkezoIranyu_ShouldThrow(EngedelyKeresTipus ellenkezoIranyuEnedelyKeresTipus)
+    {
+        // Arrange
+        var allomasNev = AllomasNev.Szechenyihegy;
+        var vonatszam = VonatTestsUtil.PARATLAN_VONATSZAM;
+        var ido = TelefonTestsUtil.TEST_IDO;
+        var nev = TelefonTestsUtil.TEST_NEV;
+        // Act and assert
+        var exception = Assert.ThrowsException<ArgumentException>(() => GrpcRequestFactory.CreateEngedelyKeresRequest(allomasNev,
+            ellenkezoIranyuEnedelyKeresTipus, null, vonatszam, ido, nev));
+        Assert.AreEqual("Az utolsó vonatnak pontosan akkor kell hiányoznia, ha azonos irányú az engedélykérés", exception.Message);
+    }
+
+    [TestMethod]
+    public void CreateEngedelyAdasRequest_WhenHasUtolsoVonatAndTipusIsAzonosIranyu_ShouldThrow()
+    {
+        // Arrange
+        var allomasNev = AllomasNev.Szechenyihegy;
+        var utolsoVonat = VonatTestsUtil.PAROS_VONATSZAM;
+        var vonatszam = VonatTestsUtil.PARATLAN_VONATSZAM;
+        var nev = TelefonTestsUtil.TEST_NEV;
+        // Act and assert
+        var exception = Assert.ThrowsException<ArgumentException>(
+            () => GrpcRequestFactory.CreateEngedelyAdasRequest(allomasNev, EngedelyAdasTipus.AzonosIranyu, utolsoVonat, vonatszam, nev));
+        Assert.AreEqual("Az utolsó vonatnak pontosan akkor kell hiányoznia, ha azonos irányú az engedélyadás", exception.Message);
+    }
+
+    [TestMethod]
+    public void CreateEngedelyAdasRequest_WhenNoUtolsoVonatAndTipusIsEllenkezoIranyu_ShouldThrow()
+    {
+        // Arrange
+        var allomasNev = AllomasNev.Szechenyihegy;
+        string vonatszam = VonatTestsUtil.PARATLAN_VONATSZAM;
+        string nev = TelefonTestsUtil.TEST_NEV;
+        // Act and assert
+        var exception = Assert.ThrowsException<ArgumentException>(
+            () => GrpcRequestFactory.CreateEngedelyAdasRequest(allomasNev, EngedelyAdasTipus.EllenkezoIranyu, null, vonatszam, nev));
+        Assert.AreEqual("Az utolsó vonatnak pontosan akkor kell hiányoznia, ha azonos irányú az engedélyadás", exception.Message);
+    }
+}
